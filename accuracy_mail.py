ea49c89575a2edf09bf2a7e12081f6b359e5c720
modi2801 <64469171+modi2801@users.noreply.github.com>
2020-05-30 04:36:14 +0530

    vdgbgr

---
+++ accuracy_mail.py
@@ -0,0 +1,8 @@
+with open("/root/accuracy.txt", "r") as f:
+ acc=f.read()
+import smtplib as sm
+msg="The model has been successfully created with the desired accuracy!! \nAccuracy of the model is: "+str(acc)+" percentage"
+server=sm.SMTP_SSL("smtp.gmail.com", 465)
+server.login("modifearless@gmail.com","bpxwivpmgmkdgghn")
+server.sendmail("modifearless@gmail.com","shhhhubhammmm@gmail.com", msg)
+server.quit()
