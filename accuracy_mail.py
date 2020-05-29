a8ec3261fb4c675ac7daf8e334141e0abd664e46
modi2801 <64469171+modi2801@users.noreply.github.com>
2020-05-30 04:40:14 +0530

    bsffre

--- accuracy_mail.py
+++ accuracy_mail.py
@@ -1,4 +1,4 @@
-with open("/root/accuracy.txt", "r") as f:
+with open("/root/task3/accuracy.txt", "r") as f:
  acc=f.read()
 import smtplib as sm
 msg="The model has been successfully created with the desired accuracy!! \nAccuracy of the model is: "+str(acc)+" percentage"
