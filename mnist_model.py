90146a662496dbfb778f7e53d403882548034f4f
modi2801 <64469171+modi2801@users.noreply.github.com>
2020-05-30 04:27:38 +0530

    cszcss

--- mnist_model.py
+++ mnist_model.py
@@ -17,7 +17,7 @@ y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
 model = Sequential()
-model.add(Dense(units=10, input_shape=(784,), activation='relu'))
+model.add(Dense(units=50, input_shape=(784,), activation='relu'))
 i=1
 n=50
 e=0
