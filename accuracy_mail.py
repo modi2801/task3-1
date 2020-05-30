4d40530841d15b473351f7c49cf76b35b525aaa3
modi2801 <64469171+modi2801@users.noreply.github.com>
2020-05-30 13:21:56 +0530

    svd d

--- accuracy_mail.py
+++ accuracy_mail.py
@@ -1,8 +1,10 @@
 with open("/root/task3/accuracy.txt", "r") as f:
  acc=f.read()
 import smtplib as sm
+from email.mime.multipart import MIMEmultipart
 msg="The model has been successfully created with the desired accuracy!! \nAccuracy of the model is: "+str(acc)+" percentage"
 server=sm.SMTP_SSL("smtp.gmail.com", 465)
-server.login("robertjr.2801@gmail.com","Modi@2801")
-server.sendmail("modifearless@gmail.com", msg)
-server.quit()
+server.ehlo()
+server.login("robertjr.2801@gmail.com","Modi@1999")
+server.sendmail("robertjr.2801@gmail.com", "modifearless@gmail.com", msg)
+server.close()
