08edaa9f9e7222daf74799ab975597bd9ed2dd39
modi2801 <64469171+modi2801@users.noreply.github.com>
2020-05-30 13:31:33 +0530

    uygyb

--- accuracy_mail.py
+++ accuracy_mail.py
@@ -4,5 +4,5 @@ import smtplib as sm
 msg="The model has been successfully created with the desired accuracy!! \nAccuracy of the model is: "+str(acc)+" percentage"
 server=sm.SMTP_SSL("smtp.gmail.com", 465)
 server.login("robertjr.2801@gmail.com","Modi@1999")
-server.sendmail("robertjr.2801@gmail.com", "modifearless@gmail.com", msg)
+server.sendmail("robertjr.2801@gmail.com","modifearless@gmail.com", msg)
 server.close()
